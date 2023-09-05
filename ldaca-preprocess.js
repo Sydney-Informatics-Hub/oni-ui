bdbfdeaa1190d924ab2c9d7b5df243a80140f851
Mike Lynch <m.lynch@sydney.edu.au>
2023-09-06 09:54:40 +1000

    preprocessor loads a crate, tested on the OMAA v1 crate

--- ldaca-preprocess.js
+++ ldaca-preprocess.js
@@ -4,14 +4,32 @@ const {ROCrate} = require('ro-crate');
 const fs = require('fs-extra');
 const yargs = require('yargs/yargs');
 
-const omaa_crate = './samples/heurist/ro-crate-medat';
+async function load_raw_crate(dir) {
+	try {
+		const crateJson = await fs.readJson(dir + '/ro-crate-metadata.json');
+		const crate = new ROCrate(crateJson);
+		return crate;
+	} catch(error) {
+		console.error("Error loading ro-crate-metadata.json");
+		console.error(error);
+		return false;
+	}
+}
 
 
 
 
-const args = yargs(process.argv.slice(2))
-	.usage('Usage: $0 -i [indir] -o [outdir]')
-	.demandOption(['i', 'o'])
-	.argv;
 
-console.log(args);
+
+(async () => {
+
+	const args = yargs(process.argv.slice(2))
+		.usage('Usage: $0 -i [indir] -o [outdir]')
+		.demandOption(['i', 'o'])
+		.argv;
+	
+	const raw_crate = await load_raw_crate(args.i);
+
+	console.log(raw_crate);
+
+})();
